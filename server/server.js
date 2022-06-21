f0e993efdfae2a75fb328f6dc017e52b7f3115d2
iamnitesh <nitesh.swarnakar1@gmail.com>
2022-06-21 14:22:07 +0545

    changed server.js file

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+dotenv.config();
 // * env variables separately placed for security concern
+// console.log(p.env);
 const process = dotenv.config().parsed;
 const PORT = process.PORT || 8000;
 const CONNECTION_URL = process.CONNECTION_URL;
